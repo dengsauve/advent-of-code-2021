e0a236da612002da741c712dc9412c1f15d1f8ff
Dennis Sauve <dengsauve@yahoo.com>
2021-12-14 21:35:08 -0800

    Day 3 part 2 Completed

--- day-3/main.rb
+++ day-3/main.rb
@@ -43,3 +43,77 @@ puts "Part 1: ", "Gamma: #{gamma_decimal}", "Epsilon: #{epsilon_decimal}",
      "Solution: #{power_consumption}"
 
 # part 2
+
+# obtain the oxygen generator rating
+numbers = input.clone
+limit = numbers[0].length
+index = 0
+
+while index < limit
+  count_ones = 0
+  count_zeroes = 0
+
+  numbers.each do |i|
+    case i[index]
+    when '1'
+      count_ones += 1
+    when '0'
+      count_zeroes += 1
+    else
+      raise('bad input')
+    end
+  end
+
+  if count_ones >= count_zeroes
+    # save nums with first bit as 1s
+    numbers.select! { |num| num[index] == '1' }
+  else
+    # save nums with first bit as 0s
+    numbers.select! { |num| num[index] == '0' }
+  end
+
+  break if numbers.length == 1
+
+  index += 1
+end
+
+oxygen_generator_rating = numbers[0].join.to_i(2)
+
+# obtain the CO2 scrubber rating
+numbers = input.clone
+limit = numbers[0].length
+index = 0
+
+while index < limit
+  count_ones = 0
+  count_zeroes = 0
+
+  numbers.each do |i|
+    case i[index]
+    when '1'
+      count_ones += 1
+    when '0'
+      count_zeroes += 1
+    else
+      raise('bad input')
+    end
+  end
+
+  if count_zeroes <= count_ones
+    # save nums with first bit as 0s
+    numbers.select! { |num| num[index] == '0' }
+  else
+    # save nums with first bit as 1s
+    numbers.select! { |num| num[index] == '1' }
+  end
+
+  break if numbers.length == 1
+
+  index += 1
+end
+
+co2_scrubber_rating = numbers[0].join.to_i(2)
+
+life_support_rating = oxygen_generator_rating * co2_scrubber_rating
+
+puts "Part 2:", "Life Support: #{life_support_rating}"
